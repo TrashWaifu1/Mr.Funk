352cee27f60ca1e43c140a3a871e5333101e7e2a
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2022-03-25 10:15:24 -0700

    the beginning of the explosion

---
+++ Mr. Funk/Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = GetComponent<Rigidbody2D>().velocity;
+        velocity = transform.TransformDirection(Vector3.right) * speed * Time.deltaTime;
+        GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            collision.gameObject.GetComponent<EnemyController>().health -= 0.5f;
+            collision.gameObject.GetComponent<EnemyController>().damage = true;
+        }
+        else
+            Destroy(gameObject);
+    }
+}
