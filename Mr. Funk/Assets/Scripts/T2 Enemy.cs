ad76907eff5909262e1fe4714e823eb4a7dc6809
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2022-04-26 10:13:32 -0700

    commented out line 32 in t2 enemy

--- Mr. Funk/Assets/Scripts/T2 Enemy.cs
+++ Mr. Funk/Assets/Scripts/T2 Enemy.cs
@@ -7,7 +7,7 @@ public class T2Enemy : MonoBehaviour
     public float speed;
     public float stoppingDistance;
     public float retreatDistance;
-    public transform player;
+    public Transform player;
     public GameObject projectile;
 
     private float timeBtwShots;
@@ -18,7 +18,7 @@ public class T2Enemy : MonoBehaviour
     void Start()
     {
 
-        player = GameObject.findGameObjectWithTag("Player").transform;
+        player = GameObject.Find("Player").transform;
         timeBtwShots = startTimeBtwShots;
     
     }
@@ -29,7 +29,7 @@ public class T2Enemy : MonoBehaviour
         
         if(Vector2.Distance(transform.position, player.position)> stoppingDistance)
         {
-            transform.position = Vector2.MoveTorwards(transform.position, player.position, speed * Time.deltaTime);
+            //transform.position = Vector2.MoveTorwards(transform.position, player.position, speed * Time.deltaTime);
         }
 
         else if(Vector2.Distance(transform.position, player.position)> stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
