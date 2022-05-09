11c0c0798f8ee80df553ba513675ed2f2444c31a
GateGamin <gategamin@gmail.com>
2022-05-09 08:49:13 -0700

    FUNKcional Button

--- Mr. Funk/Assets/Scripts/menuManager.cs
+++ Mr. Funk/Assets/Scripts/menuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menuManager : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class menuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKey)
+            SceneManager.LoadScene(1);
     }
 }
